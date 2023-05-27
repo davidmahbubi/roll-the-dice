c55f17b9dd13f31ef4de7bd4eb7fe1fc562e9a62
David Mahbubi <ulrichdavid0370@gmail.com>
2023-05-28 00:16:10 +0700

    Initial Commit

---
+++ CobaWatchOs Watch App/CobaWatchOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobaWatchOsApp.swift
+//  CobaWatchOs Watch App
+//
+//  Created by David Mahbubi on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CobaWatchOs_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
